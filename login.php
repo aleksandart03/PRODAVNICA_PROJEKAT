110896d4cde5aa5e04c54e8338cfc6398b0b4226
aleksandart03 <tomasevicaleksandar0604@gmail.com>
2025-04-07 22:57:49 +0200

    promena oko style.css

--- login.php
+++ login.php
@@ -46,11 +46,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="Stilovi/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="Stilovi/styleLogin.css?v=<?php echo time(); ?>">
 </head>
 
 <body>
-    <form method="POST" action="index.php">
+    <form method="POST" action="login.php">
         <label for="username">Korisničko ime:</label>
         <input type="text" id="username" name="username" required><br>
 
