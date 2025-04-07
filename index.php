d6c127fd44bcda423ea9ccd52fd332a8b6c80b8a
aleksandart03 <tomasevicaleksandar0604@gmail.com>
2025-04-07 17:49:49 +0200

    Pocetak login forme

--- index.php
+++ index.php
@@ -2,26 +2,6 @@
 
 require_once 'database.php';
 
-/*$password = 'admin123';
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-$sql = "INSERT INTO users (username, password, role) VALUES ('admin', '$hashed_password','admin')";
-*/
-
-/*
-$username = 'user2';
-$password = 'user234';
-
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-$sql = "INSERT INTO users (username, password, role) VALUES ('$username','$hashed_password','user')";
-
-$db = new Database();
-$conn = $db->getConnection();
-$conn->query($sql);
-
-$conn->close();
-*/
-
 ?>
 
 <!DOCTYPE html>
