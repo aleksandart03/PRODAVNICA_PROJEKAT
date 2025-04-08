f6093bd1d991964c21872c2b5e9aea50a13e46a3
aleksandart03 <tomasevicaleksandar0604@gmail.com>
2025-04-08 17:04:41 +0200

    Dodavanje index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Stilovi/style.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="naslov">Pritisnite dugme za pristup prodavnici</h1>
+
+        <a href="login.php">
+            <button>Prijavi se</button>
+        </a>
+    </div>
+</body>
+
+</html>
